29c53b2e24ea445011a990ee92505afbbf847cc2
DanielHerrera22 <159599022+DanielHerrera22@users.noreply.github.com>
2025-06-17 15:17:50 -0500

    Update index.tsx

--- Crear-Tarea/src/index.tsx
+++ Crear-Tarea/src/index.tsx
@@ -1,1 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { CrearTarea } from "./screens/CrearTarea";
 
+createRoot(document.getElementById("app") as HTMLElement).render(
+  <StrictMode>
+    <CrearTarea />
+  </StrictMode>,
+);
